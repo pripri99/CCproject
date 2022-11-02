93f92772ea7d62f7742f5486e8c915dc612081fc
joralgra <joralgra.dev@gmail.com>
2022-11-02 18:28:56 +0100

    Parallel listening

--- FE/front_end.py
+++ FE/front_end.py
@@ -52,6 +52,7 @@ if __name__ == "__main__":
     producer = KafkaProducer(bootstrap_servers=["localhost:9092"])
 
     try:
+
         # Create Kafka topic
         topic = NewTopic(name=SIGNUP_TOPIC, num_partitions=1, replication_factor=1)
         admin = KafkaAdminClient(bootstrap_servers="localhost:9092")
@@ -65,14 +66,18 @@ if __name__ == "__main__":
 
     for i in range(10):
         id = "task_" + str(i)
-        message = "Process this please"
-        createTask(id, SIGNUP_TOPIC, message,
-                   ["This", "is", "any", "kind", "of", "data"])
         taskIds.append(id)
         sleep(0.1)
 
-    #sleep(5)
+    # START LISTENER
     x = threading.Thread(target=listenToMsg, args=(taskIds,))
+
+    for identifier in taskIds:
+        message = "Process this please"
+        createTask(identifier, SIGNUP_TOPIC, message,
+                   ["This", "is", "any", "kind", "of", "data"])
+
+
     x.start()
     print("Main    : wait for the thread to finish")
     x.join()
